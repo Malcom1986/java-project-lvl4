ff43cc0635e5eece462c3f6389cb9518635fbcf7
Maksim Litvinov <maksim.litvinov@hexlet.io>
2022-06-27 18:21:58 +0300

    fix port

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,7 +5,7 @@ import io.javalin.Javalin;
 public class App {
 
     private static int getPort() {
-        var port = System.getenv().getOrDefault("PORT", 3000);
+        var port = System.getenv().getOrDefault("PORT", "3000");
         return Integer.valueOf(port);
     }
 
