2546de3541be40a1a09d1e3e1b3e76128ac3aff8
Maksim Litvinov <maksim.litvinov@hexlet.io>
2022-06-30 16:40:16 +0300

    add thymeleaf

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,6 +1,12 @@
 package hexlet.code;
 
+import hexlet.code.controllers.RootController;
 import io.javalin.Javalin;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 public class App {
 
@@ -9,14 +15,44 @@ public class App {
         return Integer.valueOf(port);
     }
 
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "develompment");
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", RootController.welcome);
+    }
+
     public static Javalin getApp() {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create(config -> {
+            if (getMode().equals("development")) {
+                config.enableDevLogging();
+            }
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
+        });
 
         app.create(config -> {
             config.enableDevLogging();
         });
 
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        addRoutes(app);
+
         return app;
     }
 
