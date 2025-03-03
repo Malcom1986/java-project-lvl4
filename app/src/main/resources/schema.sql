d65cb79b81cabdad105b31b4d8746dea79fcc02f
Maksim Litvinov <litvinovmksm@gmail.com>
2025-03-03 13:54:37 +0300

    Update schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -2,20 +2,19 @@ DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE url_checks (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
     url_id BIGINT NOT NULL,
     status_code INT NOT NULL,
     title VARCHAR(255),
     h1 VARCHAR(255),
     description TEXT,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    CONSTRAINT url_checks_url_id_foreign FOREIGN KEY (url_id) REFERENCES urls(id)
 );
 
-ALTER TABLE url_checks
-	ADD CONSTRAINT url_checks_url_id_foreign FOREIGN KEY (url_id) REFERENCES urls(id);
